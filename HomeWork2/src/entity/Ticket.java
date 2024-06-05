758e2a5aaf41abc6ce42cb4d96300ae49f05e21d
Madina <m.a.baizhanova@gmail.com>
2024-06-05 20:09:04 +0600

    Pushes the Ticket and TicketService classes

--- HomeWork2/src/entity/Ticket.java
+++ HomeWork2/src/entity/Ticket.java
@@ -1,4 +1,158 @@
-package ticket;
+package entity;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 
 public class Ticket {
+
+    public static final String ID_VALIDATION = "ID must be a maximum of 4 digits and/or chars.";
+    public static final String CONCERT_HALL_VALIDATION = "Concert hall must be a maximum of 10 chars.";
+    public static final String EVENT_CODE_VALIDATION = "Event code must be exactly 3 digits.";
+    public static final String STADIUM_SECTOR_VALIDATION = "Stadium sector must be between 'A' and 'C' including.";
+    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static final char STADIUM_SECTOR_MIN = 'A';
+    public static final char STADIUM_SECTOR_MAX = 'C';
+    public static final int EVENT_CODE_MIN = 100;
+    public static final int EVENT_CODE_MAX = 999;
+    public static final int CONCERT_HALL_MAX = 10;
+    public static final int ID_MAX = 4;
+
+    private String id;
+    private String concertHall;
+    private int eventCode;
+    private long time;
+    private boolean isPromo;
+    private char stadiumSector;
+    private double maxBackpackWeight;
+    private double price;
+    private final long creationTime;
+
+    public Ticket(String id,
+                  String concertHall,
+                  int eventCode,
+                  long time,
+                  boolean isPromo,
+                  char stadiumSector,
+                  double maxBackpackWeight,
+                  double price) {
+        setId(id);
+        setConcertHall(concertHall);
+        setEventCode(eventCode);
+        setTime(time);
+        setPromo(isPromo);
+        setStadiumSector(stadiumSector);
+        setMaxBackpackWeight(maxBackpackWeight);
+        setPrice(price);
+        this.creationTime = Instant.now().getEpochSecond();
+    }
+
+    public Ticket(String concertHall, int eventCode, long time) {
+        setConcertHall(concertHall);
+        setEventCode(eventCode);
+        setTime(time);
+        this.creationTime = Instant.now().getEpochSecond();
+    }
+
+    public Ticket() {
+        this.creationTime = Instant.now().getEpochSecond();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        if (id != null && id.length() <= ID_MAX) {
+            this.id = id;
+        } else {
+            throw new IllegalArgumentException(ID_VALIDATION);
+        }
+    }
+
+    public String getConcertHall() {
+        return concertHall;
+    }
+
+    public void setConcertHall(String concertHall) {
+        if (concertHall != null && concertHall.length() <= CONCERT_HALL_MAX) {
+            this.concertHall = concertHall;
+        } else {
+            throw new IllegalArgumentException(CONCERT_HALL_VALIDATION);
+        }
+    }
+
+    public int getEventCode() {
+        return eventCode;
+    }
+
+    public void setEventCode(int eventCode) {
+        if (eventCode >= EVENT_CODE_MIN && eventCode <= EVENT_CODE_MAX) {
+            this.eventCode = eventCode;
+        } else {
+            throw new IllegalArgumentException(EVENT_CODE_VALIDATION);
+        }
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public String getFormattedTime() {
+        return formatUnixTimestamp(this.time);
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    public boolean isPromo() {
+        return isPromo;
+    }
+
+    public void setPromo(boolean promo) {
+        isPromo = promo;
+    }
+
+    public char getStadiumSector() {
+        return stadiumSector;
+    }
+
+    public void setStadiumSector(char stadiumSector) {
+        if (stadiumSector >= STADIUM_SECTOR_MIN && stadiumSector <= STADIUM_SECTOR_MAX) {
+            this.stadiumSector = stadiumSector;
+        } else {
+            throw new IllegalArgumentException(STADIUM_SECTOR_VALIDATION);
+        }
+    }
+
+    public double getMaxBackpackWeight() {
+        return maxBackpackWeight;
+    }
+
+    public void setMaxBackpackWeight(double maxBackpackWeight) {
+        this.maxBackpackWeight = maxBackpackWeight;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public long getCreationTime() {
+        return creationTime;
+    }
+
+    public String getFormattedCreationTime() {
+        return formatUnixTimestamp(this.creationTime);
+    }
+
+    private String formatUnixTimestamp(long timestamp) {
+        return Instant.ofEpochSecond(timestamp)
+                .atZone(ZoneId.systemDefault())
+                .format(DateTimeFormatter.ofPattern(DATE_TIME_FORMAT));
+    }
 }
--- HomeWork2/src/runner/TicketService.java
+++ HomeWork2/src/runner/TicketService.java
@@ -1,4 +1,63 @@
-package ticket;
+package runner;
+
+import entity.Ticket;
 
 public class TicketService {
+
+    public static final String EMPTY_TICKET = "Empty Ticket: ";
+    public static final String LIMITED_TICKET = "Limited Ticket: ";
+    public static final String FULL_TICKET = "Full Ticket: ";
+    public static final String TICKET_CREATION_TIME = "Ticket Creation Time - ";
+    public static final String CONCERT_HALL = "Concert Hall - ";
+    public static final String EVENT_CODE = "Event Code - ";
+    public static final String TIME = "Time - ";
+    public static final String IS_PROMO = "Is Promo - ";
+    public static final String STADIUM_SECTOR = "Stadium Sector - ";
+    public static final String MAX_BACKPACK_WEIGHT_KG = "Max Backpack Weight, kg - ";
+    public static final String PRICE_$ = "Price, $ - ";
+    public static final String ID = "ID - ";
+    public static final String NEW_LINE = "\n";
+
+    public static final String EXAMPLE_ID = "4AB5";
+    public static final String EXAMPLE_CONCERT_HALL = "Hall 1";
+    public static final int EXAMPLE_TICKET_DATE_TIME = 1717999200;
+    public static final int EXAMPLE_EVENT_CODE = 111;
+    public static final char EXAMPLE_STADIUM_SECTOR = 'B';
+    public static final double EXAMPLE_MAX_BACKPACK_WEIGHT = 3.5;
+    public static final double EXAMPLE_PRICE = 59.9;
+
+    public static void main(String[] args) {
+
+        Ticket emptyTicket = new Ticket();
+        System.out.println(EMPTY_TICKET + NEW_LINE + TICKET_CREATION_TIME + emptyTicket.getFormattedCreationTime());
+        System.out.println();
+
+        Ticket limitedTicket = new Ticket(EXAMPLE_CONCERT_HALL, EXAMPLE_EVENT_CODE, EXAMPLE_TICKET_DATE_TIME);
+        System.out.println(LIMITED_TICKET + NEW_LINE
+                           + CONCERT_HALL + limitedTicket.getConcertHall() + NEW_LINE
+                           + EVENT_CODE + limitedTicket.getEventCode() + NEW_LINE
+                           + TIME + limitedTicket.getFormattedTime() + NEW_LINE
+                           + TICKET_CREATION_TIME + limitedTicket.getFormattedCreationTime());
+        System.out.println();
+
+        Ticket fullTicket = new Ticket(
+                EXAMPLE_ID,
+                EXAMPLE_CONCERT_HALL,
+                EXAMPLE_EVENT_CODE,
+                EXAMPLE_TICKET_DATE_TIME,
+                false,
+                EXAMPLE_STADIUM_SECTOR,
+                EXAMPLE_MAX_BACKPACK_WEIGHT,
+                EXAMPLE_PRICE);
+        System.out.println(FULL_TICKET + NEW_LINE
+                           + ID + fullTicket.getId() + NEW_LINE
+                           + CONCERT_HALL + fullTicket.getConcertHall() + NEW_LINE
+                           + EVENT_CODE + fullTicket.getEventCode() + NEW_LINE
+                           + TIME + fullTicket.getFormattedTime() + NEW_LINE
+                           + IS_PROMO + fullTicket.isPromo() + NEW_LINE
+                           + STADIUM_SECTOR + fullTicket.getStadiumSector() + NEW_LINE
+                           + MAX_BACKPACK_WEIGHT_KG + fullTicket.getMaxBackpackWeight() + NEW_LINE
+                           + PRICE_$ + fullTicket.getPrice() + NEW_LINE
+                           + TICKET_CREATION_TIME + fullTicket.getFormattedCreationTime());
+    }
 }
